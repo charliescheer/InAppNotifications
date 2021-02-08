28a8cbfa5cebb8d234f74383359257e47aa64cc9
Chaslie Scheer <charlie.scheer@automattic.com>
2021-02-08 18:33:03 +1300

    Added notes to IANotificationPresenter for clarity

--- IANotificationsTestProject/In App Notifications/IANotificationPresenter.swift
+++ IANotificationsTestProject/In App Notifications/IANotificationPresenter.swift
@@ -1,29 +1,44 @@
 import UIKit
 
+//IANotificationPresenter is the class that will be most frequently interacted with in SN.
+//This class will contain a queue of notifications, present them on screen, and manage animations
+//This class will also manage listening to keyboard changes and rotation changes, and manage pausing dismissal if user interacts with notification
+
 class IANotificationPresenter {
     static let shared = IANotificationPresenter()
+    
     var queue = IANQueue<IANotification>()
     var presenting = false
     
-    
     private init() { }
     
+    //Method will present a given notification on screen
+    //If a notification is already present than the nofitication is queued to be run in turn
     public func presentNotification(_ notification: IANotification) {
+        //Check if there are currently presenting notifications
         guard !presenting else {
+            //If so, enqueu the notification and exit
             queue.enqueue(notification)
             return
         }
         
+        //Set self to presenting
         presenting = true
+        
+        //Present a notification
         animateIn(notification) {
+            //When presenting is finished fade out the presented notification
             self.animateFadeOut(notification)
         }
     }
 }
 
 extension IANotificationPresenter {
+    //Animate a notification in
     public func animateIn(_ notification: IANotification, completion: @escaping () -> Void) {
+        //Make notification window visisble
         notification.window.isHidden = false
+        //Begin animation
         UIWindow.animate(withDuration: Times.fade) {
             var endFrame = notification.window.frame
             endFrame.origin.y -= 300
@@ -36,6 +51,7 @@ extension IANotificationPresenter {
     }
     
     public func animateFadeOut(_ notification: IANotification) {
+        //Choose the correct delay time
         var delay: Double
         if notification.hasAction {
             delay = Times.waitLong
@@ -43,12 +59,16 @@ extension IANotificationPresenter {
             delay = Times.waitShort
         }
         
+        //After waiting...
         DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
+            //Trigger notification to fade out
             UIWindow.animate(withDuration: Times.fade) {
                 notification.window.alpha = 0
             } completion: { (_) in
+                //When animation is complete set currently presenting to false
                 self.presenting = false
                 
+                //If the queue is not empty, trigger presentNotifications again with the next in line
                 if !self.queue.isEmpty  {
                     self.presentNotification(self.queue.dequeue()!)
                 }
