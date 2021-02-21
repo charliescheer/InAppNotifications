5ae9b6ca6d6d938e799d52ab57afe62cab59b956
Chaslie Scheer <charlie.scheer@automattic.com>
2021-02-21 14:41:26 +1300

    Refactored IANAction

--- IANotificationsTestProject/In App Notifications/IANAction.swift
+++ IANotificationsTestProject/In App Notifications/IANAction.swift
@@ -1,14 +1,6 @@
 import Foundation
 
 struct IANAction {
-    let message: String
-    var action: ((IANAction) -> Void)?
-    
-    init(message: String, action: ((IANAction) -> Void)? = nil) {
-        self.message = message
-        
-        if let action = action {
-            self.action = action
-        }
-    }
+    let title: String
+    var handler: () -> Void
 }
