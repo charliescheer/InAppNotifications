c71f24e1d24c7e85aec42f592105b5ede154e78e
Charlie Scheer <charlie.scheer@automattic.com>
2021-02-25 15:34:33 +1300

    Changed NoticePresenter responsibility to just handle displaying views

--- IANotificationsTestProject/In App Notifications/NoticePresenter.swift
+++ IANotificationsTestProject/In App Notifications/NoticePresenter.swift
@@ -3,90 +3,93 @@ import UIKit
 class NoticePresenter {
     
     // MARK: Properties
-    
-    static let shared = NoticePresenter()
-    
-    var queue = NoticeQueue<Notice>()
-    var window: UIWindow? = nil
-    var state: State = .inactive
-    
-    private init() { }
-    
-    enum State: String {
-        case inactive
-        case preparing
-        case presenting
-        case dismissing
-    }
-    
-    // MARK: Presenting Functions
-    
-    func present(_ notice: Notice) {
-        // Should check to make sure the notification doesn't already exist in queue
-        queue.enqueue(notice)
-        prepareToPresent()
-    }
-    
-    // MARK: State Management Functions
-    
-    private func prepareToPresent() {
-        guard state == .inactive else {
-            return
-        }
-        state = .preparing
-        
-        guard let noticeToPresent = queue.dequeue() else {
-            state = .inactive
-            return
-        }
-        
-        //Needs to be untouchable
-        window = UIWindow(frame: UIScreen.main.bounds)
-        let noticeViewController = NoticeViewController(notice: noticeToPresent)
-        window?.rootViewController = noticeViewController
-        
-        presentNotice()
-    }
-    
-    private func presentNotice() {
-        guard let noticeWindow = window,
-              let noticeViewController = noticeWindow.rootViewController as? NoticeViewController else {
-            state = .inactive
-            return
-        }
-        state = .presenting
+    var noticeView: NoticeView?
+    var noticeTopConstraint: NSLayoutConstraint?
 
-        noticeWindow.isHidden = false
-        
-        noticeViewController.displayNotification(completion: { () in
-            let delay = noticeViewController.notice.hasAction ? Times.waitLong : Times.waitShort
-            DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
-                self.dismissNotice()
-            }
-        })
+    var isPresenting: Bool {
+        noticeView != nil
     }
-    
-    private func dismissNotice() {
-        guard let noticeWindow = window,
-              let noticeViewController = noticeWindow.rootViewController as? NoticeViewController else {
-            state = .inactive
+
+    // MARK: Presenting/Dismissing Methods
+
+    func presentNoticeView(_ noticeView: NoticeView, completion: @escaping () -> Void) {
+        guard let keyWindow = getKeyWindow() else {
             return
         }
-        state = .dismissing
-        
-        noticeViewController.dismissNotification {
-            if self.queue.isEmpty {
-                noticeViewController.dismiss(animated: false, completion: nil)
-                self.window = nil
-                self.state = .inactive
-            } else {
-                self.prepareToPresent()
+        self.noticeView = noticeView
+        keyWindow.addSubview(noticeView)
+
+        noticeTopConstraint = noticeView.topAnchor.constraint(equalTo: keyWindow.bottomAnchor)
+        noticeTopConstraint?.isActive = true
+        NSLayoutConstraint.activate([
+            noticeView.centerXAnchor.constraint(equalTo: keyWindow.centerXAnchor),
+            noticeView.widthAnchor.constraint(equalToConstant: CGFloat(275)),
+            noticeView.heightAnchor.constraint(equalToConstant: 50)
+        ])
+
+        keyWindow.layoutIfNeeded()
+        displayNotificationView {
+            completion()
+        }
+    }
+
+    private func displayNotificationView(completion: @escaping () -> Void) {
+        guard let keyWindow = getKeyWindow(),
+              let noticeView = noticeView else {
+            return
+        }
+
+        noticeTopConstraint?.isActive = false
+        let newConstraint = noticeView.bottomAnchor.constraint(equalTo: keyWindow.bottomAnchor, constant: Constants.notificationBottomMargin)
+        newConstraint.isActive = true
+
+        let delay = noticeView.action == nil ? Times.waitShort : Times.waitLong
+
+        UIView.animate(withDuration: Times.animationTime) {
+            keyWindow.layoutIfNeeded()
+        } completion: { (_) in
+            DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
+                completion()
             }
         }
+    }
+
+    func dismissNotification(completion: @escaping () -> Void) {
+        guard let noticeView = noticeView else {
+            return
+        }
+
+        UIView.animate(withDuration: Times.animationTime) {
+            noticeView.alpha = 0
+        } completion: { (_) in
+            self.noticeView = nil
+            completion()
+        }
+    }
+}
+
+
+extension NoticePresenter {
+
+    // Convenience method to fetch current key window
+    //
+    func getKeyWindow() -> UIWindow? {
+        return UIApplication.shared.connectedScenes
+                .filter({$0.activationState == .foregroundActive})
+                .map({$0 as? UIWindowScene})
+                .compactMap({$0})
+                .first?.windows
+                .filter({$0.isKeyWindow}).first
     }
 }
 
 private struct Times {
     static let waitShort = 1.5
     static let waitLong = 2.75
+    static let animationTime = TimeInterval(0.5)
+    static let tapWait = TimeInterval(3)
+}
+
+private struct Constants {
+    static let notificationBottomMargin = CGFloat(-150)
 }
